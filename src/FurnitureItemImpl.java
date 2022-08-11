474923ae7976b4b299de18eab596f511df76e8e6
Tejas Rajan <rajan.tejas@yahoo.com>
2022-08-11 12:28:01 +0530

    Declaring objects

--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,3 +1,8 @@
-public class FurnitureItemImpl {
+import java.util.Scanner;
 
+public class FurnitureItemImpl {
+    public static void main(String[] args) {
+        FurnitureItem furnitureItem = new FurnitureItem();
+        Scanner sc = new Scanner(System.in);
+    }
 }
